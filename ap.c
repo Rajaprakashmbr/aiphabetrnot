a490f133e2885bec67c9c153462d950dd8b14c77
Rajaprakashmbr <rajaprakashmbr1996@gmail.com>
2017-07-25 13:57:17 +0530

    Create ap.c

---
+++ ap.c
@@ -0,0 +1,8 @@
+Input:  str1 = "egg", str2 = "add"
+Output: True
+'a' is mapped to 'egg' and 'add' is mapped to 'y'.
+
+Input:  str1 = "foo", str2 = "bar"
+Output: False
+Input: str1="paper", str2="title"
+output:True
